1069ea1cb99935b13c8cfb50d451f2f6e6a5ffaa
Michał Słonina <michal.slonina@gmail.com>
2025-12-30 01:13:14 +0100

    Add all project files - LibGDX game structure

---
+++ android/src/main/java/my/game/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package my.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import my.game.Main;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new Main(), configuration);
+    }
+}
